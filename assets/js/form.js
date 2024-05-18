89a268a3ff1ac77c02eb6b0cb70e76c3f2eb03f1
ndewtyme77 <dewilliams01@gmail.com>
2024-05-18 16:50:31 -0400

    added teh contact form, but need to correct some issues.

---
+++ assets/js/form.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+document.getElementById("myForm").addEventListener("submit", function(event) {
+
+  event.preventDefault();
+
+  var full_name = document.getElementById("full_name").value;
+
+  var phone = document.getElementById("phone").value;
+
+  var email = document.getElementById("email").value;
+
+  var message = document.getElementById("message").value;
+
+  
+
+  // Do something with the form data, such as sending it to a server or displaying it on the page
+
+});
+
